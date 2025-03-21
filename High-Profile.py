38bb7cec95095a090e295cc45d31d905e37da640
Cannabis-Yoyo <myworkproject215@gmail.com>
2025-03-21 16:40:05 +0500

    Update High-Profile.py

--- High-Profile.py
+++ High-Profile.py
@@ -265,7 +265,7 @@ if uploaded_file:
     
         # Set Chrome options
         chrome_options = Options()
-        chrome_options.add_argument("--headless")  # Ensure headless mode
+        chrome_options.add_argument("--headless")  # Run headless mode
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
