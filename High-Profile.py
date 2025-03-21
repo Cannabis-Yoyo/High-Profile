46aca138849858f9b29dec0ed5e00bd6316098d3
Cannabis-Yoyo <myworkproject215@gmail.com>
2025-03-21 22:02:55 +0500

    Update High-Profile.py

--- High-Profile.py
+++ High-Profile.py
@@ -257,11 +257,17 @@ if uploaded_file:
         "Topicals": "https://highprofilecannabis.com/shop/martinsville-dispensary/topicals"
     }
 
-    user_agent = (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/122.0.0.0 Safari/537.36"
-    )
+    # user_agent = (
+    #     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    #     "AppleWebKit/537.36 (KHTML, like Gecko) "
+    #     "Chrome/122.0.0.0 Safari/537.36"
+    # )
+
+    user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    ]
     
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
