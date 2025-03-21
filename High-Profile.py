75d99955239e4ad72330fdd8936125cbb50f30da
Cannabis-Yoyo <myworkproject215@gmail.com>
2025-03-21 21:13:24 +0500

    Update High-Profile.py

--- High-Profile.py
+++ High-Profile.py
@@ -263,28 +263,30 @@ if uploaded_file:
         "Chrome/122.0.0.0 Safari/537.36"
     )
 
-    import streamlit as st
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
     from webdriver_manager.chrome import ChromeDriverManager
     from webdriver_manager.core.os_manager import ChromeType
     
     @st.cache_resource
     def get_driver():
         options = Options()
-        options.add_argument("--headless")  # Run in headless mode
+        options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--remote-debugging-port=9222")  # Enable remote debugging
     
-        # Specify Chromium as the browser
-        driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        return webdriver.Chrome(executable_path=driver_path, options=options)
+        # Use Service object to manage chromedriver
+        service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+    
+        return webdriver.Chrome(service=service, options=options)
     
     driver = get_driver()
     driver.get("https://highprofilecannabis.com")
     st.code(driver.page_source)
+
     
     # from selenium import webdriver
     # from selenium.webdriver.chrome.options import Options
